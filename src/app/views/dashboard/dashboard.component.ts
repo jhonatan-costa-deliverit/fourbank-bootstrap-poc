3d8337d1caad3e6c1a7ef5ad9bbb351e1d1ca050
Jhonatan Silva da Costa <jhonatan.costa@deliverit.com.br>
2025-04-10 17:39:24 -0300

    feat(dashboard): start working in dynamic table and tickets features

--- src/app/views/dashboard/dashboard.component.ts
+++ src/app/views/dashboard/dashboard.component.ts
@@ -1,12 +1,16 @@
 import { Component } from '@angular/core';
 import {SaldosComponent} from '../../components/saldos/saldos.component';
 import {FluxoDeCaixaComponent} from '../../components/fluxo-de-caixa/fluxo-de-caixa.component';
+import {ChamadosComponent} from '../../components/chamados/chamados.component';
+import {DynamicTableComponent} from '../../components/dynamic-table/dynamic-table.component';
 
 @Component({
   selector: 'app-dashboard',
   imports: [
     SaldosComponent,
-    FluxoDeCaixaComponent
+    FluxoDeCaixaComponent,
+    ChamadosComponent,
+    DynamicTableComponent
   ],
   templateUrl: './dashboard.component.html',
   styleUrl: './dashboard.component.scss'
