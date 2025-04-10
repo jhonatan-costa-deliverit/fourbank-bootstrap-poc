eb1098a34fecaa98615dd739ef6e09071e6ebe1f
Jhonatan Silva da Costa <jhonatan.costa@deliverit.com.br>
2025-04-10 13:48:50 -0300

    feat(saldos): created a balance card

---
+++ src/app/components/saldos/saldo-card/saldo-card.component.ts
@@ -0,0 +1,33 @@
+import {Component, Input} from '@angular/core';
+import {CurrencyPipe, NgClass, NgForOf, NgIf} from '@angular/common';
+import {Router} from '@angular/router';
+
+export interface CardMenuInterface {
+  redirectLink: string,
+  linkText: string,
+}
+
+@Component({
+  selector: 'app-saldo-card',
+  imports: [
+    NgIf,
+    NgForOf,
+    NgClass,
+    CurrencyPipe
+  ],
+  templateUrl: './saldo-card.component.html',
+  styleUrl: './saldo-card.component.scss'
+})
+export class SaldoCardComponent {
+  @Input({required: true}) title: string = 'Saldo';
+  @Input({required: true}) subTitle: string = 'Limite';
+  @Input({required: true}) menuActions: CardMenuInterface[] = [];
+  @Input({required: true}) cardValue: number = 0;
+
+  constructor(private router: Router) {}
+
+  redirectToUrl(url: string): void {
+    void this.router.navigate([url]);
+  }
+
+}
--- src/app/components/saldos/saldos.component.ts
+++ src/app/components/saldos/saldos.component.ts
@@ -1,11 +1,52 @@
 import { Component } from '@angular/core';
+import {CardMenuInterface, SaldoCardComponent} from './saldo-card/saldo-card.component';
+
+export interface ValuesInterface {
+  title: string;
+  subTitle: string;
+  menuActions: CardMenuInterface[];
+  cardValue: number;
+}
 
 @Component({
   selector: 'app-saldos',
-  imports: [],
+  imports: [
+    SaldoCardComponent
+  ],
   templateUrl: './saldos.component.html',
   styleUrl: './saldos.component.scss'
 })
 export class SaldosComponent {
+  cardOne: ValuesInterface = {
+    title: 'Saldo Atual',
+    subTitle: 'Alerta de limite - R$: 500.000',
+    menuActions: [
+      {
+        linkText: 'Editar Alerta de Limite',
+        redirectLink: '#',
+      },
+      {
+        linkText: 'Detalhes',
+        redirectLink: '#',
+      }
+    ],
+    cardValue: 1340247.34
+  };
+
+  cardTwo: ValuesInterface = {
+    title: 'Aporte / Saque',
+    subTitle: 'Saldo negativo, aportar saldo',
+    menuActions: [
+      {
+        linkText: 'Aportar',
+        redirectLink: '#',
+      },
+      {
+        linkText: 'Sacar',
+        redirectLink: '#',
+      }
+    ],
+    cardValue: -500000.00
+  }
 
 }
